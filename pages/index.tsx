c65bdf94ecfab86688c3abfac67aefce833e4990
Lee Dong WOok <sh95119@gmail.com>
2023-04-07 20:57:24 +0900

    FIX: PDF 파일 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this legal case?',
+        message: '안녕하세요! 연애 코치 라이언입니다. 무엇을 도와드릴까요?',
         type: 'apiMessage',
       },
     ],
@@ -165,7 +165,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            연애 코치 라이언
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -282,7 +282,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : '당신의 대화 기록을 입력하세요.'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
--- utils/makechain.ts
+++ utils/makechain.ts
@@ -25,6 +25,36 @@ Question: {question}
 Answer in Markdown:`,
 );
 
+const QA_PROMPT_KOREAN = PromptTemplate.fromTemplate(
+    `You are an AI assistant providing helpful advice. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
+You should only provide hyperlinks that reference the context below. Do NOT make up hyperlinks.
+If you can't find the answer in the context below, just say "해당 정보는 책에서 찾을 수 없어요." Don't try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+You Should answer in Korean.
+
+Question: {question}
+=========
+{context}
+=========
+Answer in Markdown:`,
+);
+
+const QA_PROMPT_KOREAN2 = PromptTemplate.fromTemplate(
+    `You are an dating assistant providing helpful advice.
+    Talk to me like a friend.
+    You are given the following extracted parts of a long document and a question.
+    You should answer with a '냥' at the end of your sentence.
+    
+You Should answer in Korean.
+You should answer in the commanding tone of a teacher
+
+Question: {question}
+=========
+{context}
+=========
+Answer in Markdown:`,
+);
+
 export const makeChain = (
   vectorstore: PineconeStore,
   onTokenStream?: (token: string) => void,
@@ -36,18 +66,18 @@ export const makeChain = (
   const docChain = loadQAChain(
     new OpenAIChat({
       temperature: 0,
-      modelName: 'gpt-4', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
+      modelName: 'gpt-3.5-turbo', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
       streaming: Boolean(onTokenStream),
       callbackManager: onTokenStream
         ? CallbackManager.fromHandlers({
-            async handleLLMNewToken(token) {
+            async handleLLMNewToken(token, verbose=true) {
               onTokenStream(token);
               console.log(token);
             },
           })
         : undefined,
     }),
-    { prompt: QA_PROMPT },
+    { prompt: QA_PROMPT_KOREAN2 },
   );
 
   return new ChatVectorDBQAChain({
